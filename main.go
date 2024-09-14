0c21be8922ed8a7c8d5ab63fddc4356457d7cb91
EnderCHX <c@chxc.cc>
2024-09-14 12:55:38 +0800

    .

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to my API",
+		})
+	})
+
+	r.POST("/get", login)
+
+	r.Run(":8080")
+}
+
+func login(c *gin.Context) {
+	user := User{}
+	c.BindJSON(&user)
+	c.JSON(http.StatusOK, gin.H{
+		"message":  "test",
+		"username": user.Username,
+		"password": user.Password,
+	})
 }
