381a183fd7f3e834989351f9d26c3c8c48aa3f98
EnderCHX <c@chxc.cc>
2025-04-02 22:38:48 +0800

    新增访问计数相关的服务和实体结构

--- internal/domain/entity/visit.go
+++ internal/domain/entity/visit.go
@@ -1,12 +1,16 @@
 package entity
 
+import "sync"
+
 type PathCount struct {
 	FullPath string `json:"full_path" gorm:"primaryKey"`
 	Count    int    `json:"count"`
+	Lock     sync.Mutex
 }
 
 type IpCount struct {
 	Ip       string `json:"ip" gorm:"primaryKey"`
 	Count    int    `json:"count"`
 	FullPath string `json:"full_path"`
+	Lock     sync.Mutex
 }
