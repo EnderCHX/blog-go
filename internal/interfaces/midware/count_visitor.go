5e5999b85369f1f999beff9191ca6cb277ae899c
EnderCHX <c@chxc.cc>
2025-04-03 13:39:11 +0800

    fix some bugs of visit record

--- internal/interfaces/midware/count_visitor.go
+++ internal/interfaces/midware/count_visitor.go
@@ -2,27 +2,43 @@ package midware
 
 import (
 	"blog-go/internal/domain/entity"
+	"blog-go/internal/infrastructure/config"
 	"blog-go/internal/infrastructure/persistence"
+	"github.com/EnderCHX/chx-tools-go/auth"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/mssola/useragent"
 )
 
-func CountVisitor(db *persistence.DbHelper) gin.HandlerFunc {
+func CountVisitor(db *persistence.DbHelper, cf config.Config) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		go func() {
 			visitor := entity.Visit{}
 			visitor.Path = c.Request.URL.Path
 			visitor.FullPath = c.FullPath()
+			visitor.Referer = c.Request.Referer()
 			visitor.Ip = c.ClientIP()
 			visitor.UserAgent = c.Request.UserAgent()
 			ua := useragent.New(c.Request.UserAgent())
 			visitor.Browser, _ = ua.Browser()
 			visitor.OS = ua.OS()
+			visitor.Time = time.Now()
 			if ua.Mobile() {
 				visitor.Platform = "Mobile"
 			} else {
 				visitor.Platform = "Desktop"
+			}
+			token := c.GetHeader("Authorization")
+			if token != "" {
+				token = strings.Replace(token, "Bearer ", "", 1)
+				payload, err := auth.VerifyToken(token, cf.SecretKeys.AccessSecret)
+				if err == nil {
+					visitor.Username = payload.Username
+				} else {
+					visitor.Username = "GUEST"
+				}
 			}
 
 			db.VisitRepository.SaveRecord(visitor)
