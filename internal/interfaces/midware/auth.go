17b97cebbab429db20a113865643f0e29b0ef912
EnderCHX <c@chxc.cc>
2025-04-04 22:39:46 +0800

    auth support url params

--- internal/interfaces/midware/auth.go
+++ internal/interfaces/midware/auth.go
@@ -6,13 +6,17 @@ import (
 	"github.com/EnderCHX/chx-tools-go/auth"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
 func Auth(config config.Config) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("Authorization")
+		if token == "" {
+			token = c.Query("token")
+		}
 		if token != "" {
-			token = token[7:] // Bearer
+			token = strings.Replace(token, "Bearer ", "", 1)
 			claims, err := auth.VerifyToken(token, config.SecretKeys.AccessSecret)
 			if err != nil {
 				c.JSON(response.Default(http.StatusUnauthorized, false, response.CLIENT_INVALID_ACCESS_TOKEN, "token非法", nil))
